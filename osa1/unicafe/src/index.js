c23b94df776545aa36e05ebcab62d29d67ba5bb2
juhahaapalainen <juha.haapalainen@gmail.com>
2021-02-26 18:35:47 +0200

    Unicafe valmis

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -7,28 +7,82 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const handleClick = () => {
-    console.log("Painettu");
+  const handleClickGood = (props) => {
     setGood(good + 1);
+  };
+  const handleClickNeutral = (props) => {
+    setNeutral(neutral + 1);
+  };
+  const handleClickBad = (props) => {
+    setBad(bad + 1);
   };
 
   return (
     <div>
       <h1>Give feedback</h1>
-      <button onClick={handleClick} type="button">
-        good
-      </button>
-      <button onClick={handleClick} type="button">
-        neutral
-      </button>
-      <button onClick={handleClick} type="button">
-        bad
-      </button>
-      <p>{good}</p>
-      <p>{neutral}</p>
-      <p>{bad}</p>
+      <Button handleClick={() => handleClickGood()} text="good" />
+      <Button handleClick={() => handleClickNeutral()} text="neutral" />
+      <Button handleClick={() => handleClickBad()} text="bad" />
+      <div>
+        <Statistics good={good} neutral={neutral} bad={bad} />
+      </div>
     </div>
   );
+};
+
+const Statistics = (props) => {
+  if (props.good === 0 && props.neutral === 0 && props.bad === 0)
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    );
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <StatisticLine name="good" value={props.good} />
+          <StatisticLine name="neutral" value={props.neutral} />
+          <StatisticLine name="bad" value={props.bad} />
+          <StatisticLine
+            name="all"
+            value={props.good + props.neutral + props.bad}
+          />
+          <StatisticLine
+            name="average"
+            value={
+              (props.good - props.bad) /
+              (props.good + props.neutral + props.bad)
+            }
+          />
+          <StatisticLine
+            name="positive"
+            value={
+              (props.good / (props.good + props.neutral + props.bad)) * 100
+            }
+            pros="%"
+          />
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const StatisticLine = (prop) => {
+  return (
+    <tr>
+      <td>{prop.name}</td>
+      <td>
+        {prop.value} {prop.pros}
+      </td>
+    </tr>
+  );
+};
+
+const Button = (props) => {
+  return <button onClick={props.handleClick}>{props.text}</button>;
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
