231aaebf88126aa69db452f08cd757975e84a7ba
juhahaapalainen <juha.haapalainen@gmail.com>
2021-02-26 14:05:25 +0200

    unicafe v0.1

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const handleClick = () => {
+    console.log("Painettu");
+    setGood(good + 1);
+  };
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <button onClick={handleClick} type="button">
+        good
+      </button>
+      <button onClick={handleClick} type="button">
+        neutral
+      </button>
+      <button onClick={handleClick} type="button">
+        bad
+      </button>
+      <p>{good}</p>
+      <p>{neutral}</p>
+      <p>{bad}</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
