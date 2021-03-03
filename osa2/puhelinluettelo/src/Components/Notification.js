d1672c769bb02634da3afb326dde315125370690
Juha Haapalainen <juha.haapalainen@gmail.com>
2021-03-03 11:31:43 +0200

    notifications updated

--- osa2/puhelinluettelo/src/Components/Notification.js
+++ osa2/puhelinluettelo/src/Components/Notification.js
@@ -1,14 +1,11 @@
 import React from "react";
 
-const Notification = ({ message, messageType }) => {
+const Notification = ({ message }) => {
   if (message === null) {
     return null;
   }
-  if (messageType === "error") {
-    return <div className="error">{message}</div>;
-  } else {
-    return <div className="message">{message}</div>;
-  }
+
+  return <div className="message">{message}</div>;
 };
 
 export default Notification;
