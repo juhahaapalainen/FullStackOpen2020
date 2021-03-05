f7570a9fe1ea9e111beb3d60fa6c91f635ac3923
Juha Haapalainen <juha.haapalainen@gmail.com>
2021-03-05 14:03:05 +0200

    blogilistaa

---
+++ osa4/blogilista/utils/list_helper.js
@@ -0,0 +1,30 @@
+//const blog = require("../models/blog");
+
+const dummy = (blogs) => {
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  let summa = 0;
+  //console.log(blog);
+  blogs.map((blg) => (summa += blg.likes));
+  //   console.log(blogs);
+  //   console.log("LIKES", blogs.likes);
+  return summa;
+};
+
+const favoriteBlog = (blogs) => {
+  let suosituin = blogs.reduce((max, blg) => {
+    return blg.likes >= max.likes ? blg : max;
+  });
+
+  console.log("SUOSITUIN", suosituin);
+
+  return suosituin;
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+};
