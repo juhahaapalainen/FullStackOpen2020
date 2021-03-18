79367c67861ca94eb1fe7da16ecb237fba3ab38b
Juha Haapalainen <juha.haapalainen@gmail.com>
2021-03-18 14:51:22 +0200

    anekdootit 6.8

---
+++ osa6/redux-anecdotes/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './App'
+import reducer from './reducers/anecdoteReducer'
+// import { composeWithDevTools } from 'redux-devtools-extension'
+
+const store = createStore(
+  reducer, 
+  //composeWithDevTools
+  )
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
